7c56f8dfdc5faf52c2283d0e937b4b90638148f9
Louis Kaplan <j.kiszle@gmail.com>
2020-05-06 19:47:10 +0100

    nice

--- js/imgnav.js
+++ js/imgnav.js
@@ -1,43 +1,44 @@
-let Imgnav = { imgEl: document.getElementById("imgimg"),
-               imgCaption: document.getElementById("imgfigcaption"),
+let Imgnav = { curstate: 0,
                imgCaptions: ["Joe Louis and Max Schmeling fight for the first time, 1936",
                              "Joe Louis and Max Schmeling as friends years later",
                              "Jack Dempsey as a young fighter",
                              "Sugar Ray Robinson with his wife, Edna Mae Holly",
                              "Jack Johnson as a young fighter"],
-               imgFilepaths: ["../assets/boxer_joelouis_fight.jpg",
-                              "../assets/boxer_joelouis_friends.jpg",
-                              "../assets/boxer_jackdempsey.jpg",
-                              "../assets/boxer_sugarray.jpg",
-                              "../assets/boxer_jackjohnson.jpg"],
+               imgFilepaths: ["assets/boxer_joelouis_fight.jpg",
+                              "assets/boxer_joelouis_friends.jpg",
+                              "assets/boxer_jackdempsey.jpg",
+                              "assets/boxer_sugarray.jpg",
+                              "assets/boxer_jackjohnson.jpg"],
 }
 
-function upDate(previewPic) {
-    /* In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image */
-    let imagesource = previewPic.src;
-    document.getElementById('image').style.backgroundImage = `url(${imagesource})`;
-    /*
-    2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image */
-    let imagealt = previewPic.alt;
-    document.getElementById('image').innerHTML = imagealt;
+function mod(n, m) {
+    return ((n % m) + m) % m;
 }
 
-function unDo() {
-    /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image. You can use the css code to see what that original URL was */
-    document.getElementById('image').style.backgroundImage = "url('')";
-
-    /*
-    2) Change the text of the div with the id = "image" 
-    back to the original text. You can use the html code to see what that original text was */
-    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+let nowimg = function() {
+    let imgEl = document.getElementById("imgimg");
+    let figCaption = document.getElementById("imgfigcaption");
+    imgEl.src = Imgnav.imgFilepaths[Imgnav.curstate];
+    imgEl.alt = Imgnav.imgCaptions[Imgnav.curstate];
+    figCaption.innerHTML = Imgnav.imgCaptions[Imgnav.curstate];
 }
 
-let figdirs 
-let figcapts
+let nextimg = function() {
+    Imgnav.curstate = mod((Imgnav.curstate + 1), Imgnav.imgCaptions.length);
+    console.log(Imgnav.curstate);
+    nowimg();
+}
 
-function nextpicture
+let previmg = function() {
+    Imgnav.curstate = mod((Imgnav.curstate - 1), Imgnav.imgCaptions.length);
+    console.log(Imgnav.curstate);
+    nowimg();
+}
+
+let prevbutton = document.getElementById("figbackbutton");
+let flscrnbutton = document.getElementById("figfullscreen");
+let nextbutton = document.getElementById("figforwardbutton");
+
+prevbutton.addEventListener("click", previmg);
+// prevbutton.addEventListener("click", nextimg);
+nextbutton.addEventListener("click", nextimg);
