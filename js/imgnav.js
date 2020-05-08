aa567748f93ae782fd936efc213539342ab18485
Louis Kaplan <j.kiszle@gmail.com>
2020-05-08 13:51:36 +0100

    newtest

--- js/imgnav.js
+++ js/imgnav.js
@@ -9,11 +9,31 @@ let Imgnav = { curstate: 0,
                               "assets/boxer_jackdempsey.jpg",
                               "assets/boxer_sugarray.jpg",
                               "assets/boxer_jackjohnson.jpg"],
+                natWidth: [2738, 354, 384, 308, 800],
+                natHeight: [1840, 281, 545, 386, 1332]
 }
 
 function mod(n, m) {
     return ((n % m) + m) % m;
 }
+
+// let sizeimg0 = function () {
+//     let figEl = document.getElementById("imgfig");
+//     let imgEl = document.getElementById("imgimg");
+//     let figwidth = figEl.clientWidth;
+//     let figheightwithcaption = figEl.clientHeight;
+//     let figheight = figheightwithcaption - 25;
+//     let natheight = imgEl.naturalHeight;
+//     let natwidth = imgEl.naturalWidth;
+//     let aspectratio = natwidth/natheight;
+//     if (natheight > natwidth || figwidth/aspectratio > figheight) {
+//         imgEl.style.width = `${figheight*aspectratio}px`;
+//         imgEl.style.height = `${figheight}px`;
+//     } else {
+//         imgEl.style.height = `${figwidth/aspectratio}px`;
+//         imgEl.style.width = `${figwidth}px`;
+//     }
+// }
 
 let sizeimg = function () {
     let figEl = document.getElementById("imgfig");
@@ -21,24 +41,33 @@ let sizeimg = function () {
     let figwidth = figEl.clientWidth;
     let figheightwithcaption = figEl.clientHeight;
     let figheight = figheightwithcaption - 25;
-    let natheight = imgEl.naturalHeight;
-    let natwidth = imgEl.naturalWidth;
-    let aspectratio = natwidth/natheight;
-    if (natheight > natwidth || figwidth/aspectratio > figheight) {
-        imgEl.style.width = `${figheight*aspectratio}px`;
+    let natwidth = Imgnav.natWidth[Imgnav.curstate];
+    let natheight = Imgnav.natHeight[Imgnav.curstate];
+    let aspectratio = natwidth / natheight;
+    if (natheight > natwidth || figwidth / aspectratio > figheight) {
+        imgEl.style.width = `${figheight * aspectratio}px`;
         imgEl.style.height = `${figheight}px`;
     } else {
-        imgEl.style.height = `${figwidth/aspectratio}px`;
+        imgEl.style.height = `${figwidth / aspectratio}px`;
         imgEl.style.width = `${figwidth}px`;
     }
 }
 
-let nowimg = function() {
+// let nowimg0 = function() {
+//     let imgEl = document.getElementById("imgimg");
+//     let figCaption = document.getElementById("imgfigcaption");
+//     imgEl.src = Imgnav.imgFilepaths[Imgnav.curstate];
+//     imgEl.alt = Imgnav.imgCaptions[Imgnav.curstate];
+//     figCaption.innerHTML = Imgnav.imgCaptions[Imgnav.curstate];
+// }
+
+let nowimg = function () {
     let imgEl = document.getElementById("imgimg");
     let figCaption = document.getElementById("imgfigcaption");
     imgEl.src = Imgnav.imgFilepaths[Imgnav.curstate];
     imgEl.alt = Imgnav.imgCaptions[Imgnav.curstate];
     figCaption.innerHTML = Imgnav.imgCaptions[Imgnav.curstate];
+    sizeimg();
 }
 
 let nextimg = function() {
@@ -62,13 +91,13 @@ nextbutton.addEventListener("click", nextimg);
 
 // image resizing assignment
 let imgEl = document.getElementById("imgimg");
-imgEl.addEventListener("load", sizeimg);
+// imgEl.addEventListener("load", sizeimg);
 window.addEventListener("resize",sizeimg);
 
 // add initial image on document load
 let initimg = function() {
     nowimg();
-    sizeimg();
+    // sizeimg();
 }
 
 document.onload = initimg()
